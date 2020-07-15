2276cd1fe83359c499b5f0c11d6d3b459880a7e0
Josh Reeter <joshreeter@gmail.com>
2020-07-15 17:04:18 -0500

    Separate out calculation functions.

--- main.py
+++ main.py
@@ -2,43 +2,20 @@ from points import points
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from calculations import get_normalized_x, get_normalized_y
 
 
-#### Calculation Helpers
-
-def get_miles_per_degree_longitude(lat):
-    return np.cos(np.deg2rad(lat)) * 69.172
-
-
-def get_x_width(x_max, x_min, lat):
-    return (x_max - x_min) * get_miles_per_degree_longitude(lat)
-
-
-def get_y_height(y_max, y_min):
-    return (y_max - y_min) * 69.055
+# Takes an array of arrays that contain [lat,long] points, each child of the outermost array is a separate activity
+# recorded by the athlete.
+def map_dataframe(points):
+    data = np.array(points)
+    return pd.DataFrame({
+        'latitude': data[:, 0],
+        'longitude': data[:, 1]
+    })
 
 
-def get_normalized_multiplier(y_max, y_min, x_max, x_min, long):
-    return 1 / get_max_axis(y_max, y_min, x_max, x_min, long)
-
-
-def get_max_axis(y_max, y_min, x_max, x_min, lat):
-    return max(get_y_height(y_max, y_min), get_x_width(x_max, x_min, lat))
-
-
-def get_normalized_x(y_max, y_min, x_max, x_min, lat, long):
-    max_axis = get_max_axis(y_max, y_min, x_max, x_min, lat)
-    mpdl = get_miles_per_degree_longitude(lat)
-    x_width = get_x_width(x_max, x_min, lat)
-    return ((long - x_min) * mpdl + (max_axis - x_width ) / 2) * get_normalized_multiplier(y_max, y_min, x_max, x_min, lat)
-
-
-def get_normalized_y(y_max, y_min, x_max, x_min, lat, long):
-    max_axis = get_max_axis(y_max, y_min, x_max, x_min, long)
-    y_height = get_y_height(y_max, y_min)
-    return ((lat - y_min) * 69.055 + (max_axis - y_height) / 2) * get_normalized_multiplier(y_max, y_min, x_max, x_min, lat)
-
-
+# Runs normalization on an activity converting the lat, long pair to a normalized x, y pair between 0 and 1.
 def normalize(activity):
 
     normalizer = lambda lat, long: [
@@ -51,21 +28,8 @@ def normalize(activity):
     return map_dataframe(lat_long_pairs)
 
 
-# Takes an array of arrays that contain [lat,long] points, each child of the outermost array is a separate activity
-# recorded by the athlete.
-def map_dataframe(points):
-    data = np.array(points)
-    return pd.DataFrame({
-        'latitude': data[:, 0],
-        'longitude': data[:, 1]
-    })
+# Main...
 
-#### Calculation Helpers
-
-
-#### Program Work:
-
-# Create a list of dataframes for each activity.
 activities = list(map(map_dataframe, points))
 
 # Get the min/max of long/lat across all activities.
